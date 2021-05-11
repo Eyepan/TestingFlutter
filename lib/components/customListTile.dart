778923a3ae60d0b86efd0f9f8c3a5cbf86764769
Pan <68371938+Eyepan@users.noreply.github.com>
2021-05-11 11:33:28 +0530

    Fixed image resize issues

--- lib/components/customListTile.dart
+++ lib/components/customListTile.dart
@@ -3,42 +3,77 @@ import 'package:flutter_app/constants.dart' as consts;
 import 'package:flutter_app/utils/article_class.dart';
 
 Widget customListTile(Article article) {
-  print(article.imgUrl);
-  return Container(
+  if (article.imgUrl != 'null') {
+    print("Not Null, ${article.imgUrl.runtimeType} ${article.imgUrl}");
+    return Container(
       padding: EdgeInsets.all(12.0),
       margin: EdgeInsets.all(12.0),
-      height: 200.0,
+      height: 300,
       width: double.infinity,
       decoration: BoxDecoration(
         color: Colors.grey[800],
         borderRadius: BorderRadius.circular(15.0),
         boxShadow: [BoxShadow(color: Colors.grey[600], blurRadius: 4.0)],
       ),
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(article.imgUrl),
-                    fit: BoxFit.cover,
+      child: Container(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(
+                child: Center(
+                  child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Image.network(article.imgUrl != null ? article.imgUrl : "https://dummyimage.com/600x400/000/fff")
                   ),
-                  borderRadius: BorderRadius.circular(10.0)),
-            ),
-            SizedBox(height: 8.0),
-            Container(
-                padding: EdgeInsets.all(6.0),
-                decoration: BoxDecoration(
-                    color: Colors.amber[600],
-                    borderRadius: BorderRadius.circular(12.0)),
-                child: Text(article.source.name,
-                    style: TextStyle(color: Colors.white))),
-            SizedBox(height: 8.0),
-            Text(article.title,
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15.0,
-                    color: Colors.white))
-          ]));
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Container(
+                  padding: EdgeInsets.all(6.0),
+                  decoration: BoxDecoration(
+                      color: Colors.amber[600],
+                      borderRadius: BorderRadius.circular(12.0)),
+                  child: Text(article.source.name,
+                      style: TextStyle(color: Colors.white))),
+              SizedBox(height: 8.0),
+              Text(article.title,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                      color: Colors.white))
+            ]),
+      ),
+    );
+  } else {
+    return Container(
+      padding: EdgeInsets.all(12.0),
+      margin: EdgeInsets.all(12.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[800],
+        borderRadius: BorderRadius.circular(15.0),
+        boxShadow: [BoxShadow(color: Colors.grey[600], blurRadius: 4.0)],
+      ),
+      child: Container(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                  padding: EdgeInsets.all(6.0),
+                  decoration: BoxDecoration(
+                      color: Colors.amber[600],
+                      borderRadius: BorderRadius.circular(12.0)),
+                  child: Text(article.source.name,
+                      style: TextStyle(color: Colors.white))),
+              SizedBox(height: 8.0),
+              Text(article.title,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                      color: Colors.white))
+            ]),
+      ),
+    );
+  }
 }
